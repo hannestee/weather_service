4a101e45e1c3d4657d981444b9f66b57d479abf4
spongefin <tikkha1@gmail.com>
2017-05-04 14:07:01 +0300

    Add files via upload

--- weather_service/main.js
+++ weather_service/main.js
@@ -1,82 +1,66 @@
-/**
- * Created by hannest on 4/27/17.
- */
-
-console.log('test');
-
-
-//Esimerkkejä:
-var list_city = [{"Cities":
-                    [{"Helsinki": {
-                        "lon": '60',
-                        "lat": '25'
-                        }
-                    },
-                    {"Espoo": {
-                        "lon": '60',
-                        "lat": '25'
-                        }
-                    }]
-}];
+/*Esimerkkejä:
+ var list_city = [{"Cities":
+ [{"Helsinki": {
+ "lon": '60',
+ "lat": '25'
+ }
+ },
+ {"Espoo": {
+ "lon": '60',
+ "lat": '25'
+ }
+ }]
+ }];
 
 
-var list_weather = [{"Weather":
-                    {
-                        "temp":'+6c',
-                        "icon":'rain'
-                    }
-}];
+ var list_weather = [{"Weather":
+ {
+ "temp":'+6c',
+ "icon":'rain'
+ }
+ }];*/
 
-
-//console.log(list_city);
-//console.log(list_weather)
-
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+    console.log("main.js ready!");
 
     loadFavourites();
 
-    $('#favouritesButton').click(function(){
-        console.log('clicked');
+    $('#favouritesButton').click(function () {
+        //console.log('clicked');
 
         var location = $("#currentCity").text();
         var temperature = $("#temp").text();
+        var storageItem = {location: [location, temperature]};
 
-        var storage_item  = {location : [location, temperature]};
+        localStorage.setItem("favourites", JSON.stringify(storageItem));
 
-        localStorage.setItem("favourites", JSON.stringify(storage_item));
-
-        $( ".currentWeather" ).append( "<div style='border: 1px solid cyan;'><p>"+location+"</p>" +
-            "<p>"+temperature+"</p></div>" );
+        $(".currentWeather").append("<div style='border: 1px solid cyan;'><p>" + location + "</p>" +
+            "<p>" + temperature + "</p></div>");
     });
 });
 
 
 function loadFavourites() {
-    if (localStorage.getItem("favourites") !== null){
-        console.log("favourites set");
+    if (localStorage.getItem("favourites") !== null) {
+        //console.log("favourites set");
         var favourites = JSON.parse(localStorage.getItem("favourites"));
-        console.log(favourites.location[0]);
-
-
-
-
-        $( ".currentWeather" ).append( "<div style='border: 1px solid cyan;'><p>"+favourites.location[0]+"</p>" +
-            "<p>"+favourites.location[1]+"</p></div>" );
+        //console.log(favourites.location[0]);
+        $(".currentWeather").append("<div style='border: 1px solid cyan;'><p>" + favourites.location[0] + "</p>" +
+            "<p>" + favourites.location[1] + "</p></div>");
     }
 }
 
-function onLocationSelected(location, country, temp){
-    console.log('Location: ' + location);
-
+function onLocationSelected(location, country, temp) {
+    //console.log('Location: ' + location);
     /*for (city of list_city){
-        console.log(city);
-    }*/
+     console.log(city);
+     }*/
     $("#currentCity").text(location + ", " + country);
-    $("#temp").text(temp+'°C');
+    $("#temp").text(temp + '°C');
 }
 
-function setWebcam(webcamid){
-    $("iframe").attr("src", "//api.lookr.com/embed/timelapse/"+webcamid+"/day?autoresize=1");
-
+function setWebcam(webCamId) {
+    $("iframe").attr("src", "//api.lookr.com/embed/timelapse/" + webCamId + "/day?autoresize=1");
 }
+
+
--- weather_service/model.js
+++ weather_service/model.js
@@ -1,33 +1,38 @@
-
-
+var worseWeather = [[64.53, 40.54, 'Arkhangel´sk', 'Russia'], [68.43, 27.45, 'Inari', 'Finland'], [68.44, 18.08, 'Bjørnfjell', 'Norway'], [64.18, -51.72, 'Nuuk', 'Greenland'], [62.84, -69.87, 'Kimmirut', 'Canada'], [71.29, -156.78, 'Barrow', 'United States, Alaska']];
 var locations = [];
-var temperature = [];
-var typed_location;
 
-$( document ).ready(function() {
-    console.log("ready!");
+$(document).ready(function () {
+    console.log("model.js ready!");
 
-    $('#searchButton').click(function(){
-        console.log('clicked');
+    var temperature;
+
+    $('#searchButton').click(function () {
         var value = $("#location").val();
-        locations = [];
+        locations = []; //tyhjentää locations taulun
         getLocation(value);
-        typed_location = value;
     });
 
 
     function getLocation(name) {
-        //console.log('getLocation');
         $.ajax({
-            'url': 'http://api.geonames.org/search?name='+name+'&username=karjala100',
+            'url': 'http://api.geonames.org/search?name=' + name + '&username=karjala100',
             'dataType': 'xml',
             'success': onGetLocation
-            });
+        });
     }
 
+    function getPlaceName(lat, lng) {
+        $.ajax({
+            'url': 'http://api.geonames.org/findNearbyPlaceNameJSON?lat=' + lat + '&lng=' + lng + '&username=karjala100',
+            'dataType': 'json',
+            'success': function (data) {
+                console.log(data.geonames[0].name);
+                getLocation(data.geonames[0].name);
+            }
+        });
+    }
 
     function onGetLocation(data) {
-        //console.log('onGetLocation');
         $(data).find('geoname').each(function () {
 
             var id = $(this).find('geonameId').text();
@@ -36,58 +41,95 @@ $( document ).ready(function() {
             var lat = $(this).find('lat').text();
             var lng = $(this).find('lng').text();
 
-            locations.push({'id':id, 'info':[name, countryName, lat, lng]});
+            locations.push({'id': id, 'info': [name, countryName, lat, lng]});
         });
-
-        console.log(locations);
-        //console.log(typed_location);
-
-        getWeather(locations[0].info[2], locations[0].info[3]);
-        getWebcam(locations[0].info[2], locations[0].info[3]);
+        getWeather(locations[0].info[2], locations[0].info[3], 'current');
     }
 
 
-    function getWeather(lat, lng){
-        $.ajax({
-            'url': 'http://api.geonames.org/findNearByWeatherJSON?lat='+lat+'&lng='+lng+'&username=karjala100',
-            'dataType': 'json',
-            'success': showWeather
-        });
+    function getWeather(lat, lng, weatherType, place) {
+
+        if (weatherType === 'current') {
+            $.ajax({
+                'url': 'http://api.geonames.org/findNearByWeatherJSON?lat=' + lat + '&lng=' + lng + '&username=karjala100',
+                'dataType': 'json',
+                'success': showWeather
+            });
+        } else {
+            $.ajax({
+                'url': 'http://api.geonames.org/findNearByWeatherJSON?lat=' + lat + '&lng=' + lng + '&username=karjala100',
+                'dataType': 'json',
+                'success': function (data) {
+                    showWorseWeather(data, place, lat, lng);
+                }
+            });
+        }
+
     }
 
-    function getWebcam(lat, lng){
+    function getWebcam(lat, lng) {
         $.ajax({
-            url: 'https://webcamstravel.p.mashape.com/webcams/list/nearby=+'+lat+','+lng+',10', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
+            url: 'https://webcamstravel.p.mashape.com/webcams/list/nearby=+' + lat + ',' + lng + ',10', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
             type: 'GET', // The HTTP Method
             data: {}, // Additional parameters here
             datatype: 'json',
-            success: function(data) {
-                console.log(data);
-                console.log(data.result.webcams[0].id);
+            success: function (data) {
                 setWebcam(data.result.webcams[0].id);
-
             },
-            error: function(err) { console.log(err); },
-            beforeSend: function(xhr) {
+            error: function (err) {
+                console.log(err);
+            },
+            beforeSend: function (xhr) {
                 xhr.setRequestHeader("X-Mashape-Authorization", "Q6Q4WUhrnUmshp3J1WLLwlQ6ASJBp1LMEDqjsnYE2z845CFuMx"); // Enter here your Mashape key
             }
         });
     }
 
     function showWeather(data) {
-
-        $.each(data, function(){
-            console.log(data);
-           console.log(data.weatherObservation.temperature);
-           temp = data.weatherObservation.temperature;
-           /*temperature.push({'temperature': data.temperature})
-            console.log(data);*/
+        $.each(data, function () {
+            temp = data.weatherObservation.temperature;
+            temperature = temp;
         });
-
         if (locations[0] !== null) {
             onLocationSelected(locations[0].info[0], locations[0].info[1], temp);
         } else {
             onLocationSelected('Not found', 'Not found');
         }
+        setWorseWeather();
     }
+
+    function showWorseWeather(data, place, lat, lng) {
+        $.each(data, function () {
+            if (parseInt(temperature) > parseInt(data.weatherObservation.temperature)) {
+                $(".worse").show();
+                $("#worseCity").text(place);
+                $("#worseTemp").text(data.weatherObservation.temperature + '°C');
+                getWebcam(lat, lng);
+            } else {
+                $(".worse").hide();
+            }
+        });
+    }
+
+    function setWorseWeather() {
+        var rand = worseWeather[Math.floor(Math.random() * worseWeather.length)];
+        getWeather(rand[0], rand[1], '', rand[2] + ', ' + rand [3]);
+    }
+
+
+    function getGeoLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            alert("Geolocation is not supported by this browser.");
+        }
+    }
+
+    function showPosition(position) {
+        console.log("Geo coords: ", position.coords.latitude, position.coords.longitude);
+        getPlaceName(position.coords.latitude, position.coords.longitude);
+    }
+
+    getGeoLocation();
 });
+
